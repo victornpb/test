7587c51df1eeffc96e40ce4376c94fe60802ffa6
victornpb <victornpb@gmail.com>
2016-10-03 14:56:24 -0300

    Update ph.js

--- ph.js
+++ ph.js
@@ -2,18 +2,6 @@
     var self = this;
     
     console.log("PLACEHOLDER!");
-    var ns = window['LettPlaceholderObject'];
-    
-    var o = window[ns];
-    
-    
-    var account;
-    o.q.forEach(function(cmd){
-        cmd = Array.prototype.slice.call(cmd);
-        
-        var fn = cmd.splice(0,1);
-        Ph[fn].apply(Ph, cmd);
-    });
     
     var Ph = {
         setAccount : function(str){
@@ -26,5 +14,19 @@
             console.log(id, ver, container);
         },
     };
+    
+    
+    
+    var ns = window['LettPlaceholderObject'];
+    var o = window[ns];
+    
+    var account;
+    
+    o.q.forEach(function(cmd){
+        cmd = Array.prototype.slice.call(cmd);
+        
+        var fn = cmd.splice(0,1);
+        Ph[fn].apply(Ph, cmd);
+    });
     
 })();
