cf5c79e851c39899a5a78f39f76130e9a10afcc7
victornpb <victornpb@gmail.com>
2016-10-03 14:47:44 -0300

    Update ph.js

--- ph.js
+++ ph.js
@@ -1,3 +1,29 @@
 (function(){
+    var self = this;
+    
     console.log("PLACEHOLDER!");
+    var ns = window['LettPlaceholderObject'];
+    
+    var o = window[ns];
+    
+    
+    var account;
+    o.q.each(function(cmd){
+        
+        var fn = cmd.splice(0,1);
+        Ph[fn].apply(Ph, cmd);
+    });
+    
+    var Ph = {
+        setAccount : function(str){
+            console.log('setAccount');
+            console.log(str)
+            account = str;
+        },
+        showProduct : function(id, ver, container){
+            console.log('showProduct');
+            console.log(id, ver, container);
+        },
+    };
+    
 })();
